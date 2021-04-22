fbf1217715032defdec9973806fe0743e65fdd54
DilumFonseka <dilumfonsekapersnal@gmail.com>
2021-04-22 19:16:53 +0530

    Edit2

---
+++ SliitProject1/src/Subject.java
@@ -0,0 +1,29 @@
+
+public class Subject {
+private String subName;
+private int credits;
+
+public Subject(String subName,int credits) {
+	super();
+	this.subName=subName;
+	this.credits=credits;
+}
+
+public String getSubName() {
+	return subName;
+}
+
+public void setSubName(String subName) {
+	this.subName = subName;
+}
+
+public int getCredits() {
+	return credits;
+}
+
+public void setCredits(int credits) {
+	this.credits = credits;
+}
+
+
+}
